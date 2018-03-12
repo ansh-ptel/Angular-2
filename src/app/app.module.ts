6f04f8548b1a31dd094c42592bbb00e5b21fee02
Anshu Patel <patelanshu85@gmail.com>
2018-03-13 00:38:35 +0530

    Templete Driven Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material'; 
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
-
+import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,18 +28,23 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     AboutComponent,
-    ContactComponent
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [ DishService, PromotionService ],
+  providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [
+    LoginComponent
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
