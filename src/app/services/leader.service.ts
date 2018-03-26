33de6b4630e9011c81e1c102d6f9d2ab35b61a73
Anshu Patel <patelanshu85@gmail.com>
2018-03-27 01:25:44 +0530

    Promise 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -6,14 +6,25 @@ import { LEADERS } from '../shared/leaders';
 export class LeaderService {
 
   constructor() { }
-  getLeaders(): Promise<Leader[]>{
-    return Promise.resolve(LEADERS);
+  getDishes(): Promise<Leader[]> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
-  getLeader(id: number): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+
+  getDish(id: number): Promise<Leader> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(LEADERS.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
-  getFeaturedLeader(): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => (leader.featured))[0]);
+
+  getFeaturedDish(): Promise<Leader> {
+    return  new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(LEADERS.filter((dish) => dish.featured)[0]), 2000);
+    });
   }
 
 }
